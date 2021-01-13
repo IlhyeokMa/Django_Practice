14f7e41699942a815e9f2022ec15d0c9785537a0
Joachim Ma <kashas386@gmail.com>
2021-01-13 21:49:55 +0900

    admin_page start

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Fcuser(models.Model):
+    username = models.CharField(max_length=32,
+                                verbose_name='사용자명')
+    password = models.CharField(max_length=32,
+                                verbose_name='비밀번호')
+    registed_dttm = models.DateTimeField(auto_now_add=True,
+                                        verbose_name='등록시간')
+
+    def __str__(self):
+        return self.username
+    class Meta:
+        db_table = 'fastcampus_fcuser'
+        verbose_name = '테스트 페이지 사용자'
+        verbose_name_plural ='테스트 페이지 이용자'
+        
